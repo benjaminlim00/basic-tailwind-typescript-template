123ac1bf3f747a9e4e32326d8630ef158d92aaeb
benjaminlim00 <benjaminlim1996@hotmail.sg>
2020-05-29 22:19:26 +0800

    initial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FunctionComponent = () => {
+  return (
+    <div className="bg-black">
+      <h1>new react app </h1>
+    </div>
+  );
+};
+
+export default App;
