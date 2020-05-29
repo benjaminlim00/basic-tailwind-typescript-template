42fce23125bfbcf2696f56575b3c9c105164baf6
benjaminlim00 <benjaminlim1996@hotmail.sg>
2020-05-29 22:23:40 +0800

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const App: React.FunctionComponent = () => {
   return (
-    <div className="bg-black">
-      <h1>new react app </h1>
+    <div className="bg-gray-200">
+      <h1>New react app</h1>
     </div>
   );
 };
